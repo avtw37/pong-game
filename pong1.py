b0a943a72822a53cc3f98981e7b4d461b15b1a12
Abel Dimas <avtw37@gmail.com>
2020-01-06 10:01:24 -0800

    built paddle and ball collisions

--- pong1.py
+++ pong1.py
@@ -32,6 +32,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2
+ball.dy = -2
 
 # Paddle A
 def paddle_a_up():
@@ -67,4 +69,37 @@ wn.onkeypress(paddle_b_down, "Down")
 # Main Game Loop
 while True:
 	wn.update()
+
+	# Move the ball
+	ball.setx(ball.xcor() + ball.dx)
+	ball.sety(ball.ycor() + ball.dy)
+
+	# Border Checking
+	if ball.ycor() > 290:
+		ball.sety(290)
+		ball.dy *= -1
+
+	if ball.ycor() < -290:
+		ball.sety(-290)
+		ball.dy *= -1
+
+	if ball.xcor() > 390:
+		ball.goto(0, 0)
+		ball.dx *= -1
+
+	if ball.xcor() < -390:
+		ball.goto(0, 0)
+		ball.dx *= -1
+
+	# Paddle and Ball Collisions
+	if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -50):
+		ball.setx(340)
+		ball.dx *= -1
+
+	if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -50):
+		ball.setx(-340)
+		ball.dx *= -1
+
+
+
 

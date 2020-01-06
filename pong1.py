36fd144283a0908c4d6b6cf35b66d070b57ae4fd
Abel Dimas <avtw37@gmail.com>
2020-01-06 09:40:41 -0800

    built left paddle

--- pong1.py
+++ pong1.py
@@ -33,7 +33,7 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 
-# Function
+# Paddle A
 def paddle_a_up():
 	y = paddle_a.ycor()
 	y += 20
@@ -44,10 +44,24 @@ def paddle_a_down():
 	y += -20
 	paddle_a.sety(y)
 
+# Paddle B
+def paddle_b_up():
+	y = paddle_b.ycor()
+	y += 20
+	paddle_b.sety(y)
+
+def paddle_b_down():
+	y = paddle_b.ycor()
+	y += -20
+	paddle_b.sety(y)
+
 # Keyboard Binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+
 
 
 # Main Game Loop

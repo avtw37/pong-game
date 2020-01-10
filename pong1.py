415c0ef88b36ba6d1ec263c686d8b839d90e9c63
Abel Dimas <avtw37@gmail.com>
2020-01-10 10:56:19 -0800

    added score board

--- pong1.py
+++ pong1.py
@@ -7,6 +7,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_1 = 0
+score_2 = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -34,6 +38,15 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 2
 ball.dy = -2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player 1: 0 Player 2: 0", align="center", font=("Courier", 24, "normal"))
 
 # Paddle A
 def paddle_a_up():
@@ -86,10 +99,17 @@ while True:
 	if ball.xcor() > 390:
 		ball.goto(0, 0)
 		ball.dx *= -1
+		score_1 += 1
+		pen.clear()
+		pen.write("Player 1: {} Player 2: {}".format(score_1, score_2), align="center", font=("Courier", 24, "normal"))
+
 
 	if ball.xcor() < -390:
 		ball.goto(0, 0)
 		ball.dx *= -1
+		score_2 += 1
+		pen.clear()
+		pen.write("Player 1: {} Player 2: {}".format(score_1, score_2), align="center", font=("Courier", 24, "normal"))
 
 	# Paddle and Ball Collisions
 	if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -50):
